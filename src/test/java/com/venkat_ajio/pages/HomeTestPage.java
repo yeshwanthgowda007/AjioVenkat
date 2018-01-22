a7e16e6ca77cfdefb7c1ee54b5a93f3d6a0442dc
tejaamabavaram <32234236+tejaamabavaram@users.noreply.github.com>
2018-01-22 17:13:49 +0530

    Update HomeTestPage.java

--- src/test/java/com/venkat_ajio/pages/HomeTestPage.java
+++ src/test/java/com/venkat_ajio/pages/HomeTestPage.java
@@ -57,6 +57,7 @@ public class HomeTestPage extends WebDriverBaseTestPage<WebDriverTestPage> {
 		}
 
 	}
+	//select category and sub_category from menu
 	public void selectCategory(String categorytype , String subcategorytype )
 	{
 		QAFExtendedWebElement category = new QAFExtendedWebElement(String.format(ConfigurationManager.getBundle().getString("home.category.link"), categorytype));
